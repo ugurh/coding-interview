af562c663c08277123a270b88d4a13b9ac89a319
ugurh <ugur.harun@yandex.com>
2022-02-07 09:39:20 +0300

    Predicate functional

---
+++ src/main/java/java8/predicate/Person.java
@@ -0,0 +1,14 @@
+package java8.predicate;
+
+public class Person {
+
+    String name;
+    int age;
+    int yearOfService;
+
+    Person(String name, int age, int yearOfService) {
+        this.name = name;
+        this.age = age;
+        this.yearOfService = yearOfService;
+    }
+}
