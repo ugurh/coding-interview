1560166ba0869430bfed2ac3b0bdab57bb72e095
ugurh <ugur.harun@yandex.com>
2022-03-22 12:04:19 +0300

    Concurrent

--- src/main/java/java8/optional/LazyEvaluationTest.java
+++ src/main/java/java8/optional/LazyEvaluationTest.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class LazyEvaluationTest {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 
         Optional<Integer> number = data.stream()
