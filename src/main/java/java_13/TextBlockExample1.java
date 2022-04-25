91cb796a5330890750932f4e7fe731e4d794d666
ugurh <ugur.harun@yandex.com>
2022-04-25 16:01:33 +0300

    Text Blocks

---
+++ src/main/java/java_13/TextBlockExample1.java
@@ -0,0 +1,14 @@
+package java_13;
+
+public class TextBlockExample1 {
+
+    public static void main(String[] args) {
+        String textBlock = """
+                Name: Harun UGUR
+                Phone: 535056999
+                age: 18
+                """;
+
+        System.out.println(textBlock);
+    }
+}
